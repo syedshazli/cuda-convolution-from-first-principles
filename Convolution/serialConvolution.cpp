a51efa568836ae95c8ff742362a3ea12e09aa659
syedshazli <syed.abdu1818@gmail.com>
2025-10-04 20:59:20 -0400

    time to figure out indices

--- Convolution/serialConvolution.cpp
+++ Convolution/serialConvolution.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-
+// FIXME: COnvert to CPP
+// def conv2d(input, kernel, bias):
+//     kernel_shape = kernel.shape[0]
+//     output_shape = input.shape[0] - kernel_shape + 1
+//     result = np.zeros((output_shape, output_shape))
+  
+//     for row in range(input.shape[0] - 1):
+//         for col in range(input.shape[1] - 1):
+//             window = input[row: row + kernel_shape, col: col + kernel_shape]
+//             result[row, col] = np.sum(np.multiply(kernel,window))
+//     return result + bias
 void MatMulKernel()
 {
     

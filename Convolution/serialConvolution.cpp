909f8da45ddb3977354637855c57a0bb2bbb2983
syedshazli <syed.abdu1818@gmail.com>
2025-12-18 18:18:28 -0500

    serial conv works

--- Convolution/serialConvolution.cpp
+++ Convolution/serialConvolution.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
 
-// FIXME: COnvert to CPP
-// def conv2d(input, kernel, bias):
-//     kernel_shape = kernel.shape[0]
-//     output_shape = input.shape[0] - kernel_shape + 1
-//     result = np.zeros((output_shape, output_shape))
-  
-//     for row in range(input.shape[0] - 1):
-//         for col in range(input.shape[1] - 1):
-//             window = input[row: row + kernel_shape, col: col + kernel_shape]
-//             result[row, col] = np.sum(np.multiply(kernel,window))
-//     return result + bias
-// is a quadrupled nested for loop serially!
-void MatMulKernel()
-{
+// This is a known, working version on convolution, 
+// and will be used to test performance speedups and correctness of our kernel
+void convolution_cpu(int *image, int *filter, int *output,
+                     int imageWidth, int imageHeight,
+                     int filterWidth, int filterHeight) {
+    int outputWidth = imageWidth - filterWidth + 1;
+    int outputHeight = imageHeight - filterHeight + 1;
     
+    for (int outRow = 0; outRow < outputHeight; outRow++) {
+        for (int outCol = 0; outCol < outputWidth; outCol++) {
+            int sum = 0;
+            for (int fRow = 0; fRow < filterHeight; fRow++) {
+                for (int fCol = 0; fCol < filterWidth; fCol++) {
+                    int imgRow = outRow + fRow;
+                    int imgCol = outCol + fCol;
+                    sum += image[imgRow * imageWidth + imgCol] * 
+                           filter[fRow * filterWidth + fCol];
+                }
+            }
+            output[outRow * outputWidth + outCol] = sum;
+        }
+    }
 }
 int main()
 {
 
+    int image[3][6] = {
+        0, 2, 4, 6, 8, 10,
+        3, 5, 7, 9, 11,13,
+        1, 2, 4, 7, 9, 12,
+        };
+
+
+         int filter[2][2] = {
+        2, 1,
+        1, 0
+    };
+
+    int output[2][5];
+    int filterHeight = sizeof(filter)/sizeof(filter[0]);        
+    int filterWidth = sizeof(filter[0])/sizeof(filter[0][0]);
+
+    int imageWidth = sizeof(image[0])/sizeof(image[0][0]);   
+    int imageHeight = sizeof(image)/sizeof(image[0]);     
+
+    int outputLength = sizeof(output)/sizeof(output[0]);     
+    int outputWidth = sizeof(output[0])/sizeof(output[0][0]);
+
+   
+
+    convolution_cpu((int*)image, (int*)filter, (int*)output, imageWidth, imageHeight, filterWidth, filterHeight);
+
+    for(int row  = 0; row <outputLength; row++ ){
+
+           for(int col = 0; col<outputWidth; col++){//c++ XD
+
+                std::cout<<output[row][col]<<','<<' ';
+
+        }
+        std::cout<<std::endl;
+    }
 }

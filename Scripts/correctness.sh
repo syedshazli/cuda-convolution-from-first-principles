0f63d65dd0d782a4f2e52093b1ad0a77cabc6511
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-12-30 05:24:49 -0500

    Update correctness.sh

--- Scripts/correctness.sh
+++ Scripts/correctness.sh
@@ -1,3 +1,3 @@
-# bash script to run all tests fir correctnessvabd check if output is correct.
-nvcc convolution.cu -o conv
-g++ serialConvolution.cpp -o cpu
+# bash script to run all tests fir correctness and check if output is correct.
+
+# Compile code

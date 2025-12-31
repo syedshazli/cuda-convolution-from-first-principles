32fe37b6ef1c230dbd6e4ef48d74807bc30142d3
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-12-31 11:28:39 -0500

    Update correctness.sh

--- Scripts/correctness.sh
+++ Scripts/correctness.sh
@@ -1,8 +1,11 @@
 # bash script to run all tests fir correctness and check if output is correct.
 
 # Compile code for all tests
+echo COMPILING ALL TESTS
 nvcc testFiftyvyFiftyImg.cu -o fifty
 nvcc testTenbyTenImage.cu -o ten
 nvcc testHundredByHundredImg.cu -o hund
 nvcc testLargerFilter.cu -o filt
 nvcc testThousandByThousandImg.cu -o thous
+
+echo RUNNING TEN X TEN TEST
